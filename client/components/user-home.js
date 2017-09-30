3e0bc719071cb94f9db2f4b094b264a0344355d6
Ranjeet Sodhi <ranjeet@sodhi.org>
2017-09-30 13:33:16 -0400

    working UserProfile with eager loading data

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,18 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import UserDetails from './userDetails'
+import {loadUserData} from '../store'
 
 /**
  * COMPONENT
  */
-export const UserHome = (props) => {
-  const {email} = props
+export class UserHome extends Component {
+  componentDidMount(){
+    this.props.getAllUserData(this.props.user.id)
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
-  )
+  render () {
+    const {user} = this.props
+
+    const selectedEvents = []
+    const followingEvents = []
+    if (user.events !== undefined) {
+      user.events.forEach(event => {
+        (event.addociatedEvent.type === 'selected') ? selectedEvents.push(event) : followingEvents.push(event)
+      })
+      console.log('selectedEvents: ', selectedEvents)
+      console.log('followedEvents: ', followingEvents)
+    }
+    return (
+      <div>
+        <UserDetails user={user} />
+        {/* <RenderEvent followingEvent={followingEvents} /> */}
+        {/* <RenderEvent selectedEvents={selectedEvents} /> */}
+      </div>
+    )
+  }
 }
 
 /**
@@ -20,15 +39,23 @@ export const UserHome = (props) => {
  */
 const mapState = (state) => {
   return {
+    user: state.user,
     email: state.user.email
   }
 }
-
-export default connect(mapState)(UserHome)
+const mapDispatch = (dispatch) => {
+  return {
+    getAllUserData (userId) {
+      dispatch(loadUserData(userId))
+    }
+  }
+}
+export default connect(mapState, mapDispatch)(UserHome)
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
+  user: PropTypes.object,
   email: PropTypes.string
 }
