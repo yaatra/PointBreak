45485c71125c0e7690092f6334af8d842806edd9
David Eiber <davideiber@gmail.com>
2017-10-11 16:49:57 -0400

    Footer text

--- client/components/footer.js
+++ client/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="row">
         <div className="col-xs-12">
           <p>Copyright © 2017. All Rights Reserved</p>
-          <p>Made with React, Redux, Express, Sequelize; and API's from Yelp, OpenWeather and Fitbit.</p>
+          <p>Made with React/Redux, Express, Sequelize, and Yelp, OpenWeather, and Fitbit APIs.</p>
         </div>
       </div>
     </div>
