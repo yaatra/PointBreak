0f5f62aa953705bec2d110aab995c472b1de2a35
David Eiber <davideiber@gmail.com>
2017-10-12 17:57:52 -0400

    Delete event description placeholder

--- client/components/singleEventInLocation.js
+++ client/components/singleEventInLocation.js
@@ -66,30 +66,13 @@ class SingleEventInLocation extends Component {
 
             <section className="clearfix paddingAdjustTop">
               <div className="container">
-                <div className="row">
-                  <div className="col-sm-8 col-xs-12">
-                  <div className="listDetailsInfo">
-                  <div className="detailsInfoBox">
-                    <h3>About this Event</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed eiusmod tempor incididunt  labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam. </p>
-                    <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est. </p>
-                    <p>Qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui </p>
-                  </div>
-                  </div>
-                </div>
-                <div className="col-sm-4 col-xs-12">
-                {/* <div className="clearfix map-sidebar map-right">
-                  <div id="map" style={{position: "relative", margin: 0,padding: 0,height: "538px", maxWidth: "none"}}></div>
-                </div> */}
-               </div>
-              </div>
               {
                 singleEvent.id ?
                 <Chat eventId={singleEvent.id} /> :
                 null
               }
               </div>
-            </section>      
+            </section>
             <div>
 
               {
